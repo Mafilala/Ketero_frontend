d327e432b6c51c59b834f46a441048df05da850b
Maruf Sherefa <marufsherefa9@gmail.com>
2025-07-02 11:35:58 +0300

    add authWrapper

--- src/app/keeper/orders/page.tsx
+++ src/app/keeper/orders/page.tsx
@@ -1,16 +1,23 @@
+import AuthWrapper from '@/components/authWrapper/wrapper';
 import OrdersTable from '@/components/OrdersTable';
+import { Suspense } from 'react';
 
 const OrdersPage = () => {
   return (
-    <div className="">
+    <Suspense fallback={<div>Loading...</div>}>
+    <AuthWrapper>
+     <div className="">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-bold">Orders Management</h1>
       </div>
-      
+     
       <div className="">
         <OrdersTable  defaultFilter=""/>
       </div>
     </div>
+   </AuthWrapper>
+</Suspense>
+
   );
 };
 
